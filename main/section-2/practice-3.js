8b46d25b1677504b39c215110d510ac0cd2683c9
OMGLoveling <742943632@qq.com>
2017-07-30 11:45:56 +0000

    add section2 practice-3

--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,7 +1,30 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
+    let result=[];
+    
+    let reg=/\[|\]|:|-/g;
 
+    collection.forEach(function(elem){
+
+        for(var index=0;index<result.length;index++)
+    
+            {
+            if (elem.replace(reg, ',').split(",")[0] === result[index].name)
+            {
+               result[index].summary += elem.replace(reg, ",").split(",")[1] ? parseInt(elem.replace(reg,",").split(",")[1]) : 1;
+               return;
+            }
+            }
      
-  
+            
+             result[result.length] = {
+                name: elem.replace(reg, ",").split(",")[0],
+                summary: elem.replace(reg, ",").split(",")[1] ? parseInt(elem.replace(reg,',').split(",")[1]) : 1
+            }
+            
+
+         })
+
+        return result;
 }
